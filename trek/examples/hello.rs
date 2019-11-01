3bfcb614d23938d6abe4faeae19a04a637f0c8e7
Fangdun Cai <cfddream@gmail.com>
2019-11-01 17:20:46 +0800

    chore(examples): hello

--- trek/examples/hello.rs
+++ trek/examples/hello.rs
@@ -6,12 +6,14 @@ async fn main() {
     let mut app = App::new();
 
     app.router()
-        .get("/", |_| async { "Home" })
+        .get("/", |_| async { "Hello" })
         .get("/rust", |_| async { "Rust" })
         .get("/2018", |_| async { "2018" })
         .get("/users/:id", |cx: Context<()>| {
             async move { cx.params::<String>().unwrap() }
         });
+
+    // app.router().get("/hello", |_| async { "World" });
 
     let _ = app.run("127.0.0.1:8000").await;
 }
